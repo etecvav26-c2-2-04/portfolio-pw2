804a5b59aa26e3403095ae54a86fb37058b240e1
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-15 16:34:17 -0300

    Update main.php
    O código está funcional, mas tem como ser reduzido.

--- exerciciosPhP-02.03/exercicio5/main.php
+++ exerciciosPhP-02.03/exercicio5/main.php
@@ -3,5 +3,11 @@
     require_once ("functionFatorial.php");
 
     $n1 = $_GET["n1"];
-    echo fatorial($n1);
+    $n2 = $_GET["n2"];
+    $n3 = $_GET["n3"];
+    $n4 = $_GET["n4"];
+    $n5 = $_GET["n5"];
+
+    echo fatorial($n1) + fatorial($n2) + fatorial($n3) + fatorial($n4) + fatorial($n5);
+
 ?>
