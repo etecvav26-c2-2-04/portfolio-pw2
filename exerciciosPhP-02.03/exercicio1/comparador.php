10df72cbea82ed8572e1bc2ed0a5a7b88d3b643a
commits-carvalho <joaogabrielsc3@gmail.com>
2026-03-15 13:04:07 -0300

    Update comparador.php
    Adicionado as explicações das linhas do código e o comando include, melhorando a experiência de usuário.

--- exerciciosPhP-02.03/exercicio1/comparador.php
+++ exerciciosPhP-02.03/exercicio1/comparador.php
@@ -1,11 +1,17 @@
 <?php
-    $numero  = $_POST['numero'];
+    include("main.html"); //Inclui o formulário feito em HTML para a página em PHP
 
-    if ($numero > 100 && $numero < 200) {
-        echo $numero." está entre 100 e 200";
-    }
-    else {
-        echo $numero." não está entre 100 e 200";
-    }
+    $numero = $_POST["numero"];
+    /*Define que a variável numero terá seu valor definido a partir do input numero no HTML,
+    por meio do post que captura dados pelo corpo da requisição*/
 
+    if ($numero > 100 && $numero < 200) //Compara se a variavel numero está entre 100 e 200
+    { 
+        echo $numero . " está entre 100 e 200"; 
+        //Se as duas comparações são verdadeiras, exibe a mensagem
+    } else 
+    {
+        echo $numero . " não está entre 100 e 200";
+        //Se as duas comparações não são verdadeiras, exibe outra mensagem
+    }
 ?>
