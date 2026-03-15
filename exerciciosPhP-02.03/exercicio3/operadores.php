ec84b1848a91259b51f2428f055b11d7d596481b
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-15 14:31:04 -0300

    Update operadores.php
    Adicionado a funcionalidade de soma, subtração, divisão e multiplicação a partir da escolha do usuário no forms encontrado na página HTML, que está incluída neste PHP.

--- exerciciosPhP-02.03/exercicio3/operadores.php
+++ exerciciosPhP-02.03/exercicio3/operadores.php
@@ -3,6 +3,22 @@
 
     $n1 = $_GET["n1"];
     $n2 = $_GET["n2"];
-    echo $n1.$n2; //Apenas para teste da funcionalidade da variavel
 
+    $operador = $_GET["operador"];
+
+    switch($operador){
+        case '1':
+            echo "O resultado da conta é ".($n1+$n2);
+            break;
+        case '2':
+            echo "O resultado da conta é ".($n1-$n2);
+            break;
+        case '3':
+            echo "O resultado da conta é ".($n1*$n2);
+            break;
+        case '4':
+            echo "O resultado da conta é ".($n1/$n2);
+            break;
+    }
+    //switch é a melhor opção por dimuir o código, funcionando quase da mesma maneira que if else, ocupando menos linhas
 ?>
