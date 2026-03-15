57ab325b6d2608ad7eb05473079ea755bfc50bf5
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-15 16:50:41 -0300

    Update main.php
    Adicionado explicação para as linhas de código necessárias.

--- exerciciosPhP-02.03/exercicio5/main.php
+++ exerciciosPhP-02.03/exercicio5/main.php
@@ -1,13 +1,14 @@
 <?php
-    include ("main.html");
-    require_once ("functionFatorial.php");
+    include ("main.html"); //Inclui o código HTML na página PHP, para melhorar a experiência de usuário
+    require_once ("functionFatorial.php"); //Vai requerir o código incluso no arquivo
 
     $n1 = fatorial($_GET["n1"]);
     $n2 = fatorial($_GET["n2"]);
     $n3 = fatorial($_GET["n3"]);
     $n4 = fatorial($_GET["n4"]);
     $n5 = fatorial($_GET["n5"]);
+    //calculo fatorial usando o input do HTML como variável
 
     echo "A soma dos fatoriais é ".($n1+$n2+$n3+$n4+$n5)."<br>";
-
+    //Exibição final da soma de todas os fatoriais calculados
 ?>
