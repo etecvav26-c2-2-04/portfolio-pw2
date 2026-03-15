a38ad8305cb4d2040f7b4715e7c8c12a8b3169ff
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-15 16:41:04 -0300

    Update main.php
    O código segue com a mesma quantidade de linhas, porém está mais legível.

--- exerciciosPhP-02.03/exercicio5/main.php
+++ exerciciosPhP-02.03/exercicio5/main.php
@@ -2,12 +2,12 @@
     include ("main.html");
     require_once ("functionFatorial.php");
 
-    $n1 = $_GET["n1"];
-    $n2 = $_GET["n2"];
-    $n3 = $_GET["n3"];
-    $n4 = $_GET["n4"];
-    $n5 = $_GET["n5"];
+    $n1 = fatorial($_GET["n1"]);
+    $n2 = fatorial($_GET["n2"]);
+    $n3 = fatorial($_GET["n3"]);
+    $n4 = fatorial($_GET["n4"]);
+    $n5 = fatorial($_GET["n5"]);
 
-    echo fatorial($n1) + fatorial($n2) + fatorial($n3) + fatorial($n4) + fatorial($n5);
+    echo "A soma dos fatoriais é ".($n1+$n2+$n3+$n4+$n5)."<br>";
 
 ?>
