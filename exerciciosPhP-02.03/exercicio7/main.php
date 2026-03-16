c9a7fc2c02d7641b9ba351c5f2bc1868e5b66a93
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-15 22:17:24 -0300

    Update main.php
    Comentado as linhas de código e feita a maneira mais simples que encontrei de separar os valores adquiridos a partir do $_GET.

--- exerciciosPhP-02.03/exercicio7/main.php
+++ exerciciosPhP-02.03/exercicio7/main.php
@@ -1,10 +1,34 @@
 <?php
-    $numeros = array
-    (
-        $_GET["n1"],
-        $_GET["n2"],
-    );
-    foreach ($numeros as $numero) {
-        echo $numero . "<br>";
+    include("main.html");
+
+    $numeros = $_GET['num']; // Pega todos os input nomeados num e transforma em uma lista
+    $numpositivo = [];
+    $numnegativo = [];
+    //Criação de duas listas vazias para injeção de valores
+
+    foreach ($numeros as $numero) //Vai percorrer em loop os valores armazenados em $numeros
+    {
+        if ($numero >= 0)
+        {
+            $numpositivo[] = $numero; //Se o número for maior ou igual a 0 será armazenado na lista de positivos
+        }
+        else
+        {
+            $numnegativo[] = $numero; //Se não, o valor será armazenado na lista de negativos
+        }
     }
+    echo "São positivos: <br>";
+
+    foreach ($numpositivo as $positivo) //Percorre os valores na lista de positivos
+    {
+        echo $positivo."<br>"; //E os exibe
+    }
+    echo "São negativos: <br>";
+
+    foreach ($numnegativo as $negativo) //Percorre os valores na lista de negativos
+    {
+        echo $negativo."<br>"; // E os exibe
+    }
+
+
 ?>
