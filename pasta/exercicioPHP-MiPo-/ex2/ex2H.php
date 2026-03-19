f3ef1771645602c83d66a34454752f550d4f8c63
PolianySouza <polianychaves.souza@gmail.com>
2026-03-19 10:04:48 -0300

    Adicionar formulário HTML de conversão de temperatura.
    Crie um formulário de conversão de temperatura em PHP.

---
+++ pasta/exercicioPHP-MiPo-/ex2/ex2H.php
@@ -0,0 +1,49 @@
+<!--
+Data:19/03
+Nome: Poliany Chaves de Souza
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 2 - Conversão de Temperatura
+Faça um programa que leia um caractere "F" ou "C", indicando se o valor informado está em Fahrenheit ou Celsius
+Depois, o programa deve converter para a outra unidade.
+Fórmula: C = 5/9 x (F - 32)
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+    <title>Exercicio 02</title>
+</head>
+<body>
+    
+<form action="ex2.php" method="post">  
+
+<label>Insira uma temperatura</label>  
+<input type="text" name="temp">  
+
+<br><br> 
+
+<label>Informe a escala de temperatura</label> 
+<input type="text" name="escala">  
+
+<br><br>  
+
+<button>Enviar</button>  
+
+
+
+
+
+
+</form>
+
+
+
+
+
+</body>
+</html>
