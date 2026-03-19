d6f707c69b6bf4fa394f8065e5b1b813d5f60230
Gabriel Henrique Silva <gabriel.henriquegh2410@gmail.com>
2026-03-18 21:09:01 -0300

    Update index.php

--- exerciciosPhP-02.03/Exercicio2/index.php
+++ exerciciosPhP-02.03/Exercicio2/index.php
@@ -1,3 +1,14 @@
+<!--
+Data: 18/03/2026
+Autor: Gabriel Henrique
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 2 - Conversão de Temperatura
+Faça um programa que leia um caractere "F" ou "C", indicando se o valor informado está em Fahrenheit ou Celsius.
+Depois, o programa deve converter para a outra unidade.
+
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
