020f5dcac400167c25443da90ccd6122ccb7f75e
PolianySouza <polianychaves.souza@gmail.com>
2026-03-19 11:56:36 -0300

    Exercicio 03, resolver uperações.
    O arquivo sera editado para corrigir erros de código.

---
+++ pasta/exercicioPHP-MiPo-/ex2/ex3/ex3H.php
@@ -0,0 +1,60 @@
+<!---
+Data: 19/03
+Nome: Poliany Chaves de Souza
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 3 - Calculadora Aritmética
+Faça um programa que leia dois números e um operador ("+","-","*" ou "/") 
+-O programa deve mostrar o resultado da operação
+
+--->
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 03</title>
+</head>
+<body>
+    
+<form action="ex3.php" method="post">
+
+<label>Insira o primero numero:</label>
+<input type="text" name="n1">
+
+<br><br>
+
+<label>Qual operação deve ser realizada:</label>
+<input type="button" value="+">
+<input type="button" value="-">
+<input type="button" value="*">
+<input type="button" value="/">
+
+<br><br>
+
+<Label>Insira o segundo numero</Label>
+<input type="text" name="n2">
+<br><br>
+
+<button>Enviar</button>
+
+
+
+
+
+
+</form>
+
+
+
+
+
+
+
+
+</body>
+</html>
