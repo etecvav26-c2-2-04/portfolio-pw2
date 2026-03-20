d1b4c860163d4a3ce6f846d24e85914192bf934d
Gabriel Henrique Silva <gabriel.henriquegh2410@gmail.com>
2026-03-20 11:13:48 -0300

    Atualizei o index.php
    Mudei o tipo de input para "radio", para ficar mais intuitivo, passei o código do antigo "script.php", adicionei um "if" para verificar se o botão foi confirmado .

--- exerciciosPhP-02.03/Exercicio2/index.php
+++ exerciciosPhP-02.03/Exercicio2/index.php
@@ -17,16 +17,51 @@ Depois, o programa deve converter para a outra unidade.
   <title>Conversor de temperaturas</title>
 </head>
 <body>
-    <form action="script.php" method="post">
+    <form action="index.php" method="post">
+      <!-- grau -->
       <label for="">Insira o Grau:</label>  
       <br>
-      <input type="text" name="grau" placeholder="C/F">
-      <br>
+      <input type="radio" name="grau" value="C">
+      C<br>
+      <input type="radio" name="grau" value="F">
+      F<br>
+      <!-- valor -->
       <label for="">Insira o valor:</label>
       <br>
       <input type="number" name="numero" placeholder="32">
       <br>
-      <input type="submit">
+      <!-- botao -->
+      <input type="submit" name="confirmar">
     </form>
 </body>
 </html>
+
+<!-- transferi o codigo do antigo script.php, assim ficando mais compacto -->
+
+<?php
+  //verifica se o botão de confirmar foi apertado
+  if(isset($_POST["confirmar"])){
+    
+    $grau = $_POST["grau"];
+    $numero = $_POST["numero"];
+    $conversao = 0;
+    
+    //pego a posição 0 da variavel grau e transfomo em caixa alta pra evitar problemas
+    if(strtoupper($grau[0]) == "C"){
+        //calculo simples pra converter para Fahrenheit
+      $conversao = ($numero * 9/5) + 32;
+      //mostrando o valor para o usuario
+      echo "O valor em Fahrenheit é: ".$conversao;
+
+    //aqui também pego a posição 0 da variavel grau e transfomo em caixa alta pra evitar problemas
+    }elseif(strtoupper($grau[0]) == "F"){
+      //calculo para converter para Celcius
+      $conversao = ($numero - 32) * 5/9;
+      //mostrando o valor para o usuario
+      echo "O valor em Celsius é: ".$conversao;
+    }else{
+      //caso o usuario não coloque um grau valido
+      echo "Insira um grau valido";
+    }
+  }
+?>
