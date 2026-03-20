6b47404834077da495ca78ed482328cb35a6fb8d
Gabriel Henrique Silva <gabriel.henriquegh2410@gmail.com>
2026-03-20 11:27:33 -0300

    Update index.php
    Retratação:
    Como não pode haver apenas um arquivo moverei o código PHP de volta pro injustiçado "script.php" .

--- exerciciosPhP-02.03/Exercicio2/index.php
+++ exerciciosPhP-02.03/Exercicio2/index.php
@@ -34,35 +34,4 @@ Depois, o programa deve converter para a outra unidade.
       <input type="submit" name="confirmar">
     </form>
 </body>
-</html>
-
-<!-- transferi o codigo do antigo script.php, assim ficando mais compacto -->
-
-<?php
-  //verifica se o botão de confirmar foi apertado
-  if(isset($_POST["confirmar"])){
-    //verifica se um radio foi selecionado
-    if(isset($_POST["grau"])){
-
-      $grau = $_POST["grau"];
-      $numero = $_POST["numero"];
-      $conversao = 0;
-      //pego a posição 0 da variavel grau e transfomo em caixa alta pra evitar problemas
-      if(strtoupper($grau[0]) == "C"){
-          //calculo simples pra converter para Fahrenheit
-        $conversao = ($numero * 9/5) + 32;
-        //mostrando o valor para o usuario
-        echo "O valor em Fahrenheit é: ".$conversao;
-      //aqui também pego a posição 0 da variavel grau e transfomo em caixa alta pra evitar problemas
-      }elseif(strtoupper($grau[0]) == "F"){
-        //calculo para converter para Celcius
-        $conversao = ($numero - 32) * 5/9;
-        //mostrando o valor para o usuario
-        echo "O valor em Celsius é: ".$conversao;
-      }
-    }else{
-      //caso o usuario não coloque um grau valido
-      echo "Insira um grau";
-    }
-  }
-?>
+</html
