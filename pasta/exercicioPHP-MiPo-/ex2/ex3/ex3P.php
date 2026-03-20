127b70012b7e2658a5999f343f88e56ecb5e285e
PolianySouza <polianychaves.souza@gmail.com>
2026-03-20 09:48:43 -0300

    Refactor PHP arithmetic operations handling
    Comentários explicando o código adicionados.

--- pasta/exercicioPHP-MiPo-/ex2/ex3/ex3P.php
+++ pasta/exercicioPHP-MiPo-/ex2/ex3/ex3P.php
@@ -1,29 +1,27 @@
 <?php
-include("ex3.html");
+include("ex3.html"); //liga o html ao php
 
-$num1 = $_POST["n1"];
-$num2 = $_POST["n2"];
-$valor = $_POST["value"];
+$num1 = $_POST["n1"]; //associa a variavel do php a do html
+$num2 = $_POST["n2"]; //associa a variavel do php a do html
+$valor = $_POST["value"]; //associa a variavel do php a do html
 
-if( $valor = "+")
+if( $valor = "+") //se a variavel valor for +
 {
-    $resultado = $num1+$num2;
+    $resultado = $num1+$num2; //resultado é uma conta de adição das variaveis de numero
 }
-else if( $valor = "-")
+else if( $valor = "-") //se a variavel valor for -
 {
-    $resultado = $num1-$num2;
+    $resultado = $num1-$num2; //resultado é uma conta subtração das variaveis de numero
 }
-else if( $valor = "*"){
-    $resultado = $num1*$num2;
+else if( $valor = "*") //se a variavel valor for *
+{
+    $resultado = $num1*$num2; //resultado é uma conta de multiplicação das variaveis de numero
 }
-else( $valor = "/"){
-    $resultado = $num1/$num2;
+else( $valor = "/") //senão
+{
+    $resultado = $num1/$num2; //resultado é uma conta de divisão das variaveis de numero
 }
 
-echo $resultado." é o resultado da operação.";
-
-
-
-
+echo $resultado." é o resultado da operação."; //exibe o resultado da operação
 
 ?>
