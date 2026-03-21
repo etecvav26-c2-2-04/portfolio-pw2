20ed122101922d6bb8af18d5d8a6f17605d06249
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-21 18:50:27 -0300

    Update functionMediaAritmetica.php
    Adicionado comentários ao código, juntamente da exclusão do echo para a variável soma, inútil para o usuário.

--- exerciciosPhP-02.03/exercicio9/functionMediaAritmetica.php
+++ exerciciosPhP-02.03/exercicio9/functionMediaAritmetica.php
@@ -4,11 +4,13 @@
         $soma = 0;
         foreach ($numeros as $numero)
         {
-            $soma = $numero + $soma; //Por algum motivo que eu desconheço, ele não estava entendendo =+
-            echo "$soma <br>";
+            $soma = $numero + $soma; 
+            //Por algum motivo que eu desconheço, ele não estava entendendo =+
         }
 
-        $resultado = $soma / count($numeros);
+        $resultado = $soma / count($numeros); 
+        //a função do count($numeros) é contar o tamanho da array para divisão correta
+        
         return $resultado;
     }
 
