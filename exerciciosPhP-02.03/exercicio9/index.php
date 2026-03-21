5dd0118515949c81ca8451f68cb40fe6649bfa49
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-21 18:56:04 -0300

    Update index.php
    Explicado a funcionalidade do preg_split para o código.

--- exerciciosPhP-02.03/exercicio9/index.php
+++ exerciciosPhP-02.03/exercicio9/index.php
@@ -4,6 +4,11 @@
 
     $stringNumeros = $_POST['listNum'];
     $arrayNumeros = preg_split('/[,\s]+/', $stringNumeros);
+    /*
+        Fiz uma breve pesquisa e encontrei a funcionalidade preg_split:
+        A função preg_split() no PHP serve para dividir (quebrar) uma string em um array, 
+        utilizando expressões regulares (regex) como delimitador. 
+    */
     
     echo MediaAritmetica($arrayNumeros);
 ?>
