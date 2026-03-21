6bf6f5a01a8062df95d18284b3399b084dea1366
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-03-21 18:14:46 -0300

    Create main.php
    Criado o formulário na estrutura html que irá receber uma quantidade qualquer de números na forma de texto.

---
+++ exerciciosPhP-02.03/exercicio9/main.php
@@ -0,0 +1,22 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Média aritmética</title>
+</head>
+<body>
+    <h1> Calculadora média aritmética </h1>
+
+    <form action="main.php" method="POST">
+
+        <label>Insira os valores:</label>
+        <br>
+
+        <input type="text" name="listNum" placeholder="6, 6, 6" required>
+        <br>
+        <br>
+
+        <button type="submit"> Enviar </button>
+    </form>
+</body>
+</html>
+
