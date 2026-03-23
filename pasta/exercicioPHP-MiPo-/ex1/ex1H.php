14d7969b6cf3002fa83c642f7d4197ad5011f0f8
PolianySouza <polianychaves.souza@gmail.com>
2026-03-23 09:01:35 -0300

    Refactor HTML form with comments and structure
    Comentários de explicação do código adicionados.

--- pasta/exercicioPHP-MiPo-/ex1/ex1H.php
+++ pasta/exercicioPHP-MiPo-/ex1/ex1H.php
@@ -1,3 +1,12 @@
+<!--
+Data: 23/03
+Nome: Poliany Chaves de Souza
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 1 - Intervalo entre 100 e 200
+Faça um programa que receba um númerc e diga se este número está no intervalc entre 100 e 200.
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,27 +16,21 @@
 </head>
 <body>
     
-    <form action="ex1.php" method="post">
+    <form action="ex1.php" method="post"> <!--Cria um formulario-->
         
-        <label>Insira um numero:</label>
-        <input type="text" name="n1" step="any" required>
+        <label>Insira um numero:</label> <--Insere um texto-->
+        <input type="text" name="n1" step="any" required> <--Insere uma caixa de texto que recebe numeros racionais(Required faz com que seja obrigatorio inserir um numero)-->
 
-        <br><br>
+        <br><br> <--Quebra de linha-->
 
-        <label>Insira o segundo numero:</label>
-        <input type="text" name="n2" step="any" required>
+        <label>Insira o segundo numero:</label> <--Insere um texto-->
+        <input type="text" name="n2" step="any" required> <--Insere uma caixa de texto que recebe numeros racionais(Required faz com que seja obrigatorio inserir um numero)-->
 
-        <br><br>
+        <br><br> <--Quebra de linha-->
 
-        <button>Enviar</button>
+        <button>Enviar</button> <--Cria um botao-->
 
     </form>
-
-
-
-
-
-
 
 </body>
 </html>
