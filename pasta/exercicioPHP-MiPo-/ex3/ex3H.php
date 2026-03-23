788b8e6cada963f3314f642369ef79ce241e4d0b
PolianySouza <polianychaves.souza@gmail.com>
2026-03-23 09:16:51 -0300

    Refactor form structure in ex3H.php
    Erros de código serão corrigidos.

--- pasta/exercicioPHP-MiPo-/ex3/ex3H.php
+++ pasta/exercicioPHP-MiPo-/ex3/ex3H.php
@@ -1,3 +1,12 @@
+<!--
+Data: 19/03
+Nome: Poliany Chaves de Souza
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 3 - Calculadora Aritmética
+Faça um programa que leia dois números e um operador ("+","-","*" ou "/") 
+-O programa deve mostrar o resultado da operação
+-->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,30 +16,28 @@
 </head>
 <body>
     
-<form action="ex3.php" method="post">
+    <form action="ex3P.php" method="post">
+    
+        <label>Insira o primero numero:</label> <!--Insere um texto-->
+        <input type="text" name="n1" step="any" required> <!--Insere uma caixa de texto obrigatoria que recebe numeros decimais-->
+        
+        <br><br> <!--Quebra de linha-->
+        
+        <label>Qual operação deve ser realizada:</label> <!--Insere um texto-->
+        <input type="button" value="+">
+        <input type="button" value="-">
+        <input type="button" value="*">
+        <input type="button" value="/">
+        
+        <br><br>
+        
+        <Label>Insira o segundo numero</Label> <!--Insere um texto-->
+        <input type="text" name="n2" step="any" required> <!--Insere uma caixa de texto obrigatoria que recebe numeros decimais-->
+        <br><br>
+        
+        <button>Enviar</button>
+        
+    </form>
 
-<label>Insira o primero numero:</label>
-<input type="text" name="n1">
-
-<br><br>
-
-<label>Qual operação deve ser realizada:</label>
-<input type="button" value="+">
-<input type="button" value="-">
-<input type="button" value="*">
-<input type="button" value="/">
-
-<br><br>
-
-<Label>Insira o segundo numero</Label>
-<input type="text" name="n2">
-<br><br>
-
-<button>Enviar</button>
-
-
-
-
-
-
-</form>
+</body>
+</html>
