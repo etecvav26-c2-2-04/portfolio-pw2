7b18f40473019bac65d3cdb4647d47acd19feefa
PolianySouza <polianychaves.souza@gmail.com>
2026-03-23 08:49:46 -0300

    Add HTML form for number input in ex1H.php
    Exercicio 01, recebe dois números e exibe sua soma.

---
+++ pasta/exercicioPHP-MiPo-/ex1/ex1H.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 01</title>
+</head>
+<body>
+    
+    <form action="ex1.php" method="post">
+        
+        <label>Insira um numero:</label>
+        <input type="text" name="n1" step="any" required>
+
+        <br><br>
+
+        <label>Insira o segundo numero:</label>
+        <input type="text" name="n2" step="any" required>
+
+        <br><br>
+
+        <button>Enviar</button>
+
+    </form>
+
+
+
+
+
+
+
+</body>
+</html>
