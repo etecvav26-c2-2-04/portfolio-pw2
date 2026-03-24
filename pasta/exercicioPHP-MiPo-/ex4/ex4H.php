5bf02f97ae124feea1eda31e2e74494bdd4f9b87
PolianySouza <polianychaves.souza@gmail.com>
2026-03-24 08:40:12 -0300

    Refactor form HTML structure and add required attribute
    Comentários de explicação do código adicionados.

--- pasta/exercicioPHP-MiPo-/ex4/ex4H.php
+++ pasta/exercicioPHP-MiPo-/ex4/ex4H.php
@@ -1,3 +1,18 @@
+<!--
+Data: 24/03
+Nome: Poliany Chaves de Souza
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 4 - Triângulo Numérico
+Leia um número n e imprima n linhas nc seguinte formato (exemplo para n = 6)
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+-->
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,16 +22,12 @@
 </head>
 <body>
     
-    <form action="ex4P.php" method="post">
-        <label>Insira um numero:</label>
-        <input type="text" name="num">
-        <button>Enviar</button>
-
-
+    <form action="ex4P.php" method="post"> <!--Cria um formulario-->
+        <label>Insira um numero:</label> <!--Insere um texto-->
+        <input type="text" name="num" required> <!--Insere uma caixa de texto obrigatoria-->
+        <button>Enviar</button> <!--Insere um botao-->
 
     </form>
-
-
 
 </body>
 </html>
