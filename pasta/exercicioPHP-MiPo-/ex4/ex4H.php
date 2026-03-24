9be41a069fce467b8247c754172fed90f484b2cb
PolianySouza <polianychaves.souza@gmail.com>
2026-03-24 08:32:43 -0300

    Add HTML form for Exercise 04 in ex4H.php
    Exercicio 04, cria um triângulo numérico com o número recebido.

---
+++ pasta/exercicioPHP-MiPo-/ex4/ex4H.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Exercicio 04</title>
+</head>
+<body>
+    
+    <form action="ex4P.php" method="post">
+        <label>Insira um numero:</label>
+        <input type="text" name="num">
+        <button>Enviar</button>
+
+
+
+    </form>
+
+
+
+</body>
+</html>
