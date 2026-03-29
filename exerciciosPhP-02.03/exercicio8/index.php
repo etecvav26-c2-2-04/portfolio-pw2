e0b8221664283d4de563ed705a348db6f822fef9
Gabriel Henrique Silva <gabriel.henriquegh2410@gmail.com>
2026-03-29 11:03:03 -0300

    adicionando o exercicio 8

---
+++ exerciciosPhP-02.03/exercicio8/index.php
@@ -0,0 +1,29 @@
+
+<!--
+Data: 20/03/2026
+Autor: Gabriel Henrique
+Objetivo: Aprofundar os conhecimentos em HTML e PHP
+
+Exercício 8 - Soma de 0 até N com Função
+Crie uma função soma(n) que receba um número inteiro e retorne a soma de 0 até esse número.
+
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Soma</title>
+</head>
+<body>
+    <form action="script.php" method="post">
+        <label>
+        Insira um numero:<br>
+        <input type="number" name="numero" placeholder="12">
+        </label>
+        <input type="submit">
+    </form>
+</body>
+</html>
+
