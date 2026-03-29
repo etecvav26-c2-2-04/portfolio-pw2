8b56f6aeb7b31696aeaa687a503ca7b9f0273f48
PolianySouza <polianychaves.souza@gmail.com>
2026-03-29 10:51:41 -0300

    Change included file from ex3.html to ex3H.php
    Updated the included HTML file and added comments for clarity.

--- exercicioPHP-MiPo-/ex3/ex3P.php
+++ exercicioPHP-MiPo-/ex3/ex3P.php
@@ -1,9 +1,9 @@
 <?php
-include("ex3.html");
+include("ex3H.php"); //Liga o html ao php
 
-$num1 = $_POST["n1"];
-$num2 = $_POST["n2"];
-$valor = $_POST["value"];
+$num1 = $_POST["n1"]; //Associa a variavel do php a do html
+$num2 = $_POST["n2"]; //Associa a variavel do php a do html
+$valor = $_POST["value"]; //Associa a variavel do php a do html
 
 if( $valor == "+") //se a variavel valor for +
 {
