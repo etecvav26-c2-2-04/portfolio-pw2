1603c5c72688b4f0979ed07ba14766416840593a
Milena <milenakuryy@gmail.com>
2026-04-09 16:02:53 -0300

    Fix echo statement for displaying sum result

--- exercicioPHP-MiPo-/ex1/ex1P.php
+++ exercicioPHP-MiPo-/ex1/ex1P.php
@@ -3,6 +3,6 @@ include("ex1H.php"); //liga o html ao php//
 $n1 = $_POST["n1"]; //associa a variavel do php a do hml//
 $n2 = $_POST["n2"]; //associa a variavel do php a do hml//
 
-echo "A somo das numeros que voce inseriu é: ".$n1 + $n2; //exibe o resultado da soma das variaveis//
+echo "A soma das numeros que voce inseriu é: ".$n1 + $n2; //exibe o resultado da soma das variaveis//
 
 ?>
