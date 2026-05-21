8def29c852d49b7dbbd2eba5371fe5ea93168fb3
etecvav26-c2-2-04 <buflinho8@gmail.com>
2026-05-21 17:14:51 -0300

    Criação da tabela.
    Adicionado a query para a criação da tabela no servidor.

--- 2-bim/farmacia/database.sql
+++ 2-bim/farmacia/database.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE Produtos
+(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(100) NOT NULL,
+    Fabricante VARCHAR(100) NOT NULL,
+    Preco DECIMAL (10,2) NOT NULL,
+    Estoque INT NOT NULL
+)
+/*Query do sql para a criação da tabela no servidor*/
