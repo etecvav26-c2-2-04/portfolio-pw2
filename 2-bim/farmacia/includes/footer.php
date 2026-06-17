e7428ae77dc3274bf1a18eb772f88e5c21aa02e6
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-06-17 15:20:08 -0300

    Update footer.php
    Criado o footer para ser adicionado no index.php.

---
+++ 2-bim/farmacia/includes/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+<footer>
+    <p> © 2026 João G.S Carvalho, Gabriel H. Silva. Todos os direitos reservados.</p>
+</footer>
+</html>
