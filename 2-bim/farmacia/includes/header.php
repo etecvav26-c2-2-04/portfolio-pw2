0bbec82ba3197195087a882dda004af2d638aace
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-06-17 15:17:45 -0300

    Update header.php
    Criado o header para o include no index.php.

---
+++ 2-bim/farmacia/includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar">
+        <ul class="nav-links">
+            <li><a href="index.php">Produtos</a></li>
+            <li><a href="cadastro.php" class="active">Cadastrar</a></li>
+            <li><a href="editar.php">Editar</a></li>
+            <li><a href="excluir.php">Excluir</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
