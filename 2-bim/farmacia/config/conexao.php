f052851caab7b6c3d38ff85a6bd2df3c0916ce3e
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-06-17 15:15:28 -0300

    Update conexao.php
    Correção de erros e atualização da conexao com o banco de dados Mysql do proprio php localhost.

--- 2-bim/farmacia/config/conexao.php
+++ 2-bim/farmacia/config/conexao.php
@@ -1,7 +1,14 @@
-<<?php 
+<?php 
 $dsn = "mysql:host=localhost;dbname=mysql;charset=utf8";
 $usuario = "root";
 $senha = "";
 
 //dsn significa Data Source Name (Nome da Fonte de Dados).
-?>>
+
+try {
+    $pdo = new PDO($dsn, $usuario, $senha);
+} catch (PDOException $e) 
+{ 
+    die("Erro ao conectar: " . $e->getMessage());
+}
+?>
