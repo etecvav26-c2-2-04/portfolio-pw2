17f26d6b1f124d78db4d12fa9cd7951eec3eaa0d
PolianySouza <polianychaves.souza@gmail.com>
2026-06-18 14:35:52 -0300

    Criado a possibilidade de excluir.

--- 2-bim/farmacia/excluir.php
+++ 2-bim/farmacia/excluir.php
@@ -1,1 +1,15 @@
 
+<?php
+require_once("config/conexao.php");
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+
+        $sql = "DELETE FROM produtos WHERE id = :id";
+            $stmt = $pdo->prepare($sql);
+                $stmt->execute([':id' => $id]);
+                }
+
+header("Location: index.php");
+exit();
+?>
