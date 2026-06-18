cf716847bb9a16c066ab5f6450a8f81a63bb8f28
Milena <milenakuryy@gmail.com>
2026-06-18 14:07:23 -0300

    Atualizado o header e alterado algumas linhas em função do CSS

--- 2-bim/farmacia/includes/header.php
+++ 2-bim/farmacia/includes/header.php
@@ -1,18 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> FarmAura - Sistema de Estoque</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <nav class="navbar">
         <ul class="nav-links">
             <li><a href="index.php">Produtos</a></li>
-            <li><a href="cadastro.php" class="active">Cadastrar</a></li>
-            <li><a href="editar.php">Editar</a></li>
-            <li><a href="excluir.php">Excluir</a></li>
+            <li><a href="cadastro.php">Cadastrar Novo</a></li>
         </ul>
     </nav>
-</body>
-</html>
+    <div class="container">
