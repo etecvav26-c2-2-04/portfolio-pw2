2afa5fd10ffd0c056f724e2556ce1309cff7ffd1
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-06-18 13:59:22 -0300

    Update footer.php
    Atualizado o nome dos participantes do grupo.

--- 2-bim/farmacia/includes/footer.php
+++ 2-bim/farmacia/includes/footer.php
@@ -9,6 +9,6 @@
 
 </body>
 <footer>
-    <p> © 2026 João G.S Carvalho, Gabriel H. Silva. Todos os direitos reservados.</p>
+    <p> © 2026 Gabriel H. Silva, João G. S. Carvalho, Milena K. Trindade, Poliany C. Souza Todos os direitos reservados.</p>
 </footer>
 </html>
