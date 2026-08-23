20c05c55578ae89aeec1cabdda2c8a5a2984fd80
PolianySouza <polianychaves.souza@gmail.com>
2026-08-23 16:51:26 -0300

    alterações simples no código

---
+++ 3-bim/TrabalhoPesquisa/Index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pesquisa CriptografiasPHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body >
+    <?php include 'includes/header.php'; ?>
+    
+    <div class="mx-auto card" style=" margin: 5%; width: 75%;">
+
+        <div class="card-header">Resumo da Pesquisa</div>
+
+        <div class="card-body">
+
+            <p class="card-text">
+                <h4> Resumo da Pesquisa </h4>
+                Para este estudo, selecionamos três das bibliotecas e abordagens mais consolidadas e amplamente utilizadas no mercado de desenvolvimento PHP: <br>
+                É importante ressaltar que este material possui um caráter estritamente educacional. Para fins didáticos e para facilitar o entendimento do processo de reversão e verificação dos algoritmos, o projeto apresentará a senha descriptografada diretamente na interface do site.
+                <ul>
+                    <li> 
+                        OpenSSL: Uma biblioteca robusta que fornece diversas funções de criptografia 
+                        simétrica e assimétrica, amplamente utilizada para comunicação segura e manipulação de certificados.
+                    </li>
+
+                    <li>   
+                        Sodium Crypt: Considerada o padrão moderno para criptografia no PHP 
+                        (introduzida como extensão nativa no PHP 7.2), oferece uma API de alto nível, segura por padrão e resistente a erros de implementação comum.
+                    </li>
+
+                    <li>
+                        Hash (Funções de Hashing): Focada em funções de dispersão unidirecionais (como password_hash e bcrypt),
+                        essenciais para o armazenamento seguro de senhas e integridade de dados.
+                    </li>b
+                </ul>
+                <h5> Objetivo Didático </h5>
+                Em ambientes de produção reais, a exibição de senhas descriptografadas ou o armazenamento de dados sensíveis em texto puro é estritamente contraindicado, devendo-se seguir as melhores práticas de segurança da informação.
+            <p>
+        </div>
+    </div>
+    
+    <?php include 'includes/footer.php'?>
+</body>
+</html>
--- 3-bim/TrabalhoPesquisa/Sodium.php
+++ 3-bim/TrabalhoPesquisa/Sodium.php
@@ -4,24 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pesquisa PHP</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-<link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="style.css">
     
 </head>
 <body>
-    <?php include 'navBar.php'; ?>
-        <div class="container mt-4">
+    <?php include 'includes/header.php'; ?>
+
+    <div class="container mt-4">
         <div class="card text-bg-ligth mb-4">
             <div class="card-header">O que é o Sodium?</div>
 
             <div class="card-body">
 
                 <p class="card-text">
-                  A <strong>Libsodium (ou Sodium)</strong> funciona como um <strong>cofre inteligente pré-configurado</strong>: ela combina algoritmos matemáticos modernos para <strong>trancar dados</strong> com chaves secretas, </strong>embaralhar senhas</strong> de forma irreversível e </strong>assinar mensagens</strong> para provar sua autoria, sem deixar margem para erros do programador.
+                    <strong> Sodium</strong> funciona como um <strong>cofre inteligente pré-configurado</strong>: ela combina algoritmos matemáticos modernos para <strong>trancar dados</strong> com chaves secretas, </strong>embaralhar senhas</strong> de forma irreversível e </strong>assinar mensagens</strong> para provar sua autoria, sem deixar margem para erros do programador.
             </div>
         </div>
     
-                    <!-- Formulário -->
         <form method="post" action="">
             <div class="row g-3 align-items-center">
 
@@ -30,23 +32,16 @@
                 </div>
 
                 <div class="col-auto">
-                    <input type="password" id="inputPassword6" name="SenhaHash" class="form-control" required>
+                    <input type="password" id="inputPassword6" name="SenhaSodium" class="form-control" required>
                 </div>
 
                 <div class="col-auto">
                     <button class="btn btn-primary" type="submit">Enviar</button>
                 </div>
-                <div class="col-12 mt-3">
-                    <?php
-                        if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['SenhaHash'])) {
-                            $senha = $_POST['SenhaHash'];
-                            $senhaHash = password_hash($senha, PASSWORD_DEFAULT);
-                            echo "<strong>Senha criptografada com sodium:</strong><br>",($senhaHash);
-                        }
-                    ?>
-                </div>
             </div>
         </form>
     </div>
+
+    <?php include 'includes/footer.php'?>
 </body>
 </html>
