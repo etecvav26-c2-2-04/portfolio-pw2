db7388d44a9d7faeae6cb4781601644ceda4f130
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-08-23 16:41:39 -0300

    Create footer.php
    Rodapé do site. Usei funções do bootstrap para fazer com que o corpo do site ocupe todo o espaço da tela para o rodapé não flutuar no meio da página.

---
+++ 3-bim/TrabalhoPesquisa/includes/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="d-flex flex-column min-vh-100">
+        
+</body>
+
+<footer class = "bg-body-tertiary text-center text-lg-start py-3 border-top mt-auto">
+    <div class="container text-center">
+        <span class="text-muted">&copy; <?php echo date('Y'); ?> Criptografias. Todos os direitos reservados - João G. S. Carvalho & Poliany C. Souza.</span>
+    </div>
+</footer>
+</html>
