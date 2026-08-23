3986c70fdfcedfcbd5c9c62dc2402f28b196661f
João Gabriel da Silva Carvalho <joaogabrielsc3@gmail.com>
2026-08-23 16:53:54 -0300

    Update header.php
    Atualização no navbar para que a função de navegação entre as páginas se torne possível

--- 3-bim/TrabalhoPesquisa/includes/header.php
+++ 3-bim/TrabalhoPesquisa/includes/header.php
@@ -10,7 +10,7 @@
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="Pesquisa.php">Criptografias</a>
+            <a class="navbar-brand" href="Index.php">Criptografias</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -19,16 +19,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">OpenSSL</a>
+                        <a class="nav-link active" aria-current="page" href="SSL.php">OpenSSL</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Sodium</a>
+                        <a class="nav-link active" aria-current="page" href="Sodium.php">Sodium</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Hash</a>
+                        <a class="nav-link active" aria-current="page" href="Hash.php">Hash</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Crypt</a>
+                        <a class="nav-link active" aria-current="page" href="Crypt.php">Crypt</a>
                     </li>
                 </ul>
             </div>
