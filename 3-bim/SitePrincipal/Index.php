1f0f6a598bab812a9e4784180dbb4f2d985c2246
PolianySouza <polianychaves.souza@gmail.com>
2026-08-27 10:51:04 -0300

    O site está incompleto
    Ainda falta a funcionalidade para implementação de valores no sql. A pagina de login terá de conversar com o BD o que ainda não acontece, pois não há nada na mesma.

---
+++ 3-bim/SitePrincipal/Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
+    crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="float-end">
+        <a class="btn btn-primary m-1" href="login.php" role="button">Login</a>
+        <a class="btn btn-primary m-1" href="cadastro.php" role="button"> Cadastro</a>
+    </div>
+</body>
+</html>
---
+++ 3-bim/SitePrincipal/cadastro.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link 
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
+    crossorigin="anonymous">
+
+    <link href="style.css" rel="stylesheet">
+</head>
+<body>
+    <form class="row g-3 m-5">
+
+        <div class="col-md-6">
+            <label for="inputEmail" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail" placeholder="MariaAntonieta1792@gmail.com" required>
+        </div>
+
+        <div class="col-md-6">
+            <label for="inputPassword" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="inputPassword" required>
+        </div>
+
+        <div class="col-md-4">
+            <label for="inputName" class="form-label">Nome</label>
+            <input type="text" class="form-control" id="inputName" required>
+        </div>
+
+        <div class="col-md-4">
+            <label for="inputCPF" class="form-label">CPF</label>
+            <input type="text" class="form-control" id="inputCPF" placeholder="584.404.728-11" required>
+        </div>
+
+        <div class="col-md-4">
+            <label for="inputCelular" class="form-label">Celular (opcional)</label>
+            <input type="tel" class="form-control" id="inputCelular" placeholder="(11) 92876-8977">
+        </div>
+
+        <div class="col-md-6">
+            <label for="inputAddress" class="form-label">Endereço</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="Rua dos bobos Nº0" required>
+        </div>
+
+        <div class="col-md-3">
+            <label for="inputState" class="form-label">Estado</label>
+            <select id="inputState" class="form-select" required>
+                <option value="" selected disabled>Escolha...</option>
+                <option value="AC">AC</option>
+                <option value="AL">AL</option>
+                <option value="AP">AP</option>
+                <option value="AM">AM</option>
+                <option value="BA">BA</option>
+                <option value="CE">CE</option>
+                <option value="DF">DF</option>
+                <option value="ES">ES</option>
+                <option value="GO">GO</option>
+                <option value="MA">MA</option>
+                <option value="MT">MT</option>
+                <option value="MS">MS</option>
+                <option value="MG">MG</option>
+                <option value="PA">PA</option>
+                <option value="PB">PB</option>
+                <option value="PR">PR</option>
+                <option value="PE">PE</option>
+                <option value="PI">PI</option>
+                <option value="RJ">RJ</option>
+                <option value="RN">RN</option>
+                <option value="RS">RS</option>
+                <option value="RO">RO</option>
+                <option value="RR">RR</option>
+                <option value="SC">SC</option>
+                <option value="SP">SP</option>
+                <option value="SE">SE</option>
+                <option value="TO">TO</option>
+            </select>
+        </div>
+        
+        <div class="col-md-3">
+            <label for="inputCity" class="form-label">Cidade</label>
+            <input type="text" class="form-control" id="inputCity" required>
+        </div>
+
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="gridCheck" required>
+                <label class="form-check-label" for="gridCheck">
+                    Aceito os termos e condições
+                </label>
+            </div>
+        </div>
+
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+        </div>
+
+    </form>
+</body>
+</html>
