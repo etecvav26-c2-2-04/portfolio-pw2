056af33d08f08e745e50d7919acfdf0fd5d5845a
Gabriel Henrique Silva <gabriel.henriquegh2410@gmail.com>
2026-09-17 17:11:38 -0300

    Add files via upload

---
+++ 3-bim/SitePrincipal/partials/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="Index.php">EntreLinhas</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item"><a class="nav-link" href="Loja.php">Loja</a></li>
+        <li class="nav-item"><a class="nav-link" href="Produtos.php">Gerenciar Produtos</a></li>
+        <li class="nav-item"><a class="nav-link" href="Carrinho.php">Carrinho</a></li>
+        <li class="nav-item"><a class="nav-link" href="Usuarios.php">Usuários</a></li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="FormCadastro.php">Cadastro</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
